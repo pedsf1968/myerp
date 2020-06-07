e88e7bab862fe22b29c53f5c06237a023d7a69fa
PEDSF <pe.dsf@free.fr>
2020-06-07 18:16:51 +0200

    Correction bug configuration dans sqlContext

--- myerp-consumer/src/test/java/com/dummy/myerp/consumer/dao/impl/db/dao/SequenceEcritureComptableDaoImplTest.java
+++ myerp-consumer/src/test/java/com/dummy/myerp/consumer/dao/impl/db/dao/SequenceEcritureComptableDaoImplTest.java
@@ -2,6 +2,8 @@ package com.dummy.myerp.consumer.dao.impl.db.dao;
 
 import com.dummy.myerp.model.bean.comptabilite.SequenceEcritureComptable;
 import org.junit.jupiter.api.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Date;
 
@@ -9,64 +11,115 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.fail;
 
 class SequenceEcritureComptableDaoImplTest extends ConsumerTestCase {
-
-   private static Date date;
+   private Logger logger = LoggerFactory.getLogger(SequenceEcritureComptableDaoImplTest.class);
    private static Integer annee;
-   private static SequenceEcritureComptable vSequenceEcritureComptable;
-
-   @BeforeAll
-   public static void beforeAll(){
-      date = new Date();
-   }
+   private static String codeJournal;
+   private static Integer derniereValeur;
+   private static SequenceEcritureComptable vSEC;
 
    @BeforeEach
    public void beforeEach(){
-      vSequenceEcritureComptable = new SequenceEcritureComptable();
+      vSEC = new SequenceEcritureComptable();
    }
 
    @Test
    @Tag("getLastSeqOfTheYear")
    @DisplayName("Verify that we get the last SequenceEcritureComptable")
    public void getLastSeqOfTheYear(){
+      annee = 2016;
+      codeJournal = "AC";
+      derniereValeur = 40;
 
-
-      SequenceEcritureComptable vSEC = getDaoProxy().getComptabiliteDao().
-            getLastSeqOfTheYear(2016, "OD");
+      vSEC = getDaoProxy().getComptabiliteDao().
+            getLastSeqOfTheYear(annee, codeJournal);
 
       assertThat(vSEC).isNotNull();
-      assertThat(vSEC.getAnnee()).isEqualTo(2016);
-      assertThat(vSEC.getDerniereValeur()).isEqualTo(88);
+      assertThat(vSEC.getAnnee()).isEqualTo(annee);
+      assertThat(vSEC.getDerniereValeur()).isEqualTo(derniereValeur);
    }
 
+   @Test
+   @Tag("getLastSeqOfTheYear")
+   @DisplayName("Verify that we get null if the sequence year doesn't exist")
+   public void getLastSeqOfTheYear_returnNull_ofNewSequenceYear(){
+      annee = 2025;
+      codeJournal = "AC";
+
+      vSEC = getDaoProxy().getComptabiliteDao().getLastSeqOfTheYear(annee, codeJournal);
+
+      assertThat(vSEC).isNull();
+   }
+
+   @Test
+   @Tag("getLastSeqOfTheYear")
+   @DisplayName("Verify that we get null if the journal doesn't exist")
+   public void getLastSeqOfTheYear_returnNull_ofNewSequenceJournal(){
+      annee = 2016;
+      codeJournal = "ZZ";
+
+      vSEC = getDaoProxy().getComptabiliteDao().getLastSeqOfTheYear(annee, codeJournal);
+
+      assertThat(vSEC).isNull();
+   }
 
    @Test
    @Tag("insertSequenceEcritureComptable")
-   @Disabled
    public void insertSequenceEcritureComptable(){
       try{
-         SequenceEcritureComptable vSEC = new SequenceEcritureComptable();
-         vSEC.setAnnee(2018);
-         vSEC.setDerniereValeur(104);
-         getDaoProxy().getComptabiliteDao().insertSequenceEcritureComptable(vSEC, "BQ");
-      }catch(Exception e){
-         fail("L'insertion de la sequence comptable a echoué");
+         annee = 2020;
+         codeJournal = "AC";
+         derniereValeur = 111;
+
+         vSEC.setAnnee(annee);
+         vSEC.setDerniereValeur(111);
+         getDaoProxy().getComptabiliteDao().insertSequenceEcritureComptable(vSEC, codeJournal);
+      }catch(Exception exception){
+         logger.error("Insertion of  SequenceEcritureComptable failed cause : ",exception);
+         fail();
       }
 
+      vSEC = getDaoProxy().getComptabiliteDao().getLastSeqOfTheYear(annee, codeJournal);
 
+      assertThat(vSEC.getAnnee()).isEqualTo(annee);
+      assertThat(vSEC.getDerniereValeur()).isEqualTo(derniereValeur);
    }
 
    @Test
    @Tag("updateSequenceEcritureComptable")
-   @Disabled
+   @DisplayName("Verify that we update the SequenceEcritureComptable")
    public void updateSequenceEcritureComptable(){
+      annee = 2016;
+      codeJournal = "AC";
+      derniereValeur = 555;
+      Integer oldValue;
+
       try{
-         SequenceEcritureComptable vSeq = getDaoProxy().getComptabiliteDao().getLastSeqOfTheYear(2016, "OD");
-         vSeq.setAnnee(vSeq.getAnnee()+2);
-         vSeq.setDerniereValeur(vSeq.getDerniereValeur()+2);
-         getDaoProxy().getComptabiliteDao().updateSequenceEcritureComptable(vSeq, "OD");
-      }catch (Exception e){
-         fail("La mise a jour de la sequence comptable a echoué");
+         vSEC = getDaoProxy().getComptabiliteDao().getLastSeqOfTheYear(annee, codeJournal);
+         oldValue = vSEC.getDerniereValeur();
+         vSEC.setDerniereValeur(derniereValeur);
+         getDaoProxy().getComptabiliteDao().updateSequenceEcritureComptable(vSEC, codeJournal);
+
+         vSEC = getDaoProxy().getComptabiliteDao().getLastSeqOfTheYear(annee, codeJournal);
+
+         assertThat(vSEC.getAnnee()).isEqualTo(annee);
+         assertThat(vSEC.getDerniereValeur()).isEqualTo(derniereValeur);
+
+         // restore oldvalue
+         vSEC.setDerniereValeur(oldValue);
+         getDaoProxy().getComptabiliteDao().updateSequenceEcritureComptable(vSEC, codeJournal);
+
+         vSEC = getDaoProxy().getComptabiliteDao().getLastSeqOfTheYear(annee, codeJournal);
+
+         assertThat(vSEC.getAnnee()).isEqualTo(annee);
+         assertThat(vSEC.getDerniereValeur()).isEqualTo(oldValue);
+
+
+      }catch (Exception exception){
+         logger.error("Update of SequenceEcritureComptable failed cause :", exception);
+         fail();
       }
+
+
   }
 
 }
